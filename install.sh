8929aed4663b4635988567ac2d80e9ec4d7b1055
ndndndn1 <datahj@nnfc.re.kr>
2026-02-04 18:06:03 +0900

    Update install script to use temporary setup file
    Download setup script to temporary location, execute it, and remove afterward. Switch to user 'u1' after setup.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-curl -fsSL https://gist.githubusercontent.com/ndndndn1/3adda44b2a8a1f54bbf5dc4afdfa6212/raw/set_user_claude.sh | bash
-source ~/.bashrc
+curl -fsSL https://gist.githubusercontent.com/ndndndn1/3adda44b2a8a1f54bbf5dc4afdfa6212/raw/set_user_claude.sh -o /tmp/setup.sh
+bash /tmp/setup.sh
+rm /tmp/setup.sh
+
+# user change
+exec su - u1
