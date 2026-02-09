5f748ff4406f03831a2d804cbfc64a01751b94ef
ndndndn1 <datahj@nnfc.re.kr>
2026-02-09 15:07:52 +0900

    Update install.sh for user-specific script execution
    Modify install.sh to use different scripts based on user type.

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
-curl -fsSL https://gist.githubusercontent.com/ndndndn1/3adda44b2a8a1f54bbf5dc4afdfa6212/raw/set_user_claude.sh -o /tmp/setup.sh
-bash /tmp/setup.sh
-rm /tmp/setup.sh
+GIST_BASE="https://gist.githubusercontent.com/ndndndn1"
 
+if [ "$(id -u)" -eq 0 ]; then
+    echo "terminal root"
+    curl -fsSL "$GIST_BASE/3adda44b2a8a1f54bbf5dc4afdfa6212/raw/set_user_claude.sh" -o /tmp/setup.sh
+else
+    echo "terminal user ($(whoami))"
+    curl -fsSL "$GIST_BASE/b0ac876fd66a30ec48e5467d3650536b/raw/set_claude.sh" -o /tmp/setup.sh
+fi
+
+bash /tmp/setup.sh
+rm -f /tmp/setup.sh
 # user change. oo
 # su - u1
